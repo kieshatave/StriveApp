6d46c1b76f55fc3b965027ab2622d3742b355c05
kieshatave <kiesha.seymone@icloud.com>
2025-01-17 17:24:25 -0600

    updates

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
