e0cc7f321fb59a9d4a53f554bec35ec93d7782eb
kieshatave <kiesha.seymone@icloud.com>
2025-01-17 17:08:59 -0600

    removing sens data - database uri

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:0h8j70bYUivDWshK@workoutappfree.f6osj.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connected');
     } catch (error) {
         console.error(error.message);
